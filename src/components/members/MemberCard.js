caedcfc481fcfc7a5a868712cc5da9c7a742cd7e
AJ de Leon <ajdeleon1064@gmail.com>
2018-02-06 12:35:11 -0500

    reorder members and v-align divs top in memberlist

--- src/components/members/MemberCard.js
+++ src/components/members/MemberCard.js
@@ -3,7 +3,7 @@ import temp from '../../assets/amex_banner_small.png'
 
 const MemberCard = ({ name, bio }) => {
   return (
-    <div className="dib ba b--site-dark br2 w-30-l w-40-m w-80-s ph2-ns bg-white mh1-ns mt4-ns mt2 ph3 ph4-l">
+    <div className="dib ba b--site-dark br2 w-30-l w-40-m w-80-s ph2-ns bg-white mh1-ns mt4-ns mt2 ph3 ph4-l v-top">
         <div className="w4 mt2 center"><img src={temp} alt="tempMember" /></div>
         <div className="f4 b tc mt2 site-blue">{name}</div>
         <hr className="mw3 bb bw1 b--site-dark o-50" />
--- src/components/members/Members.js
+++ src/components/members/Members.js
@@ -4,20 +4,20 @@ import MemberCard from './MemberCard'
 const sampleBio = "Duis et libero non risus varius dapibus a nec nunc. Nulla sed massa in purus posuere hendrerit. Suspendisse id dictum justo, vitae accumsan justo. Nullam at ex rhoncus, posuere mauris malesuada, pretium justo. Ut porttitor eleifend velit, sodales sagittis augue euismod vitae. Etiam sit amet elit rutrum, accumsan ipsum ut, rhoncus nunc. Aenean eu massa vitae odio mollis pellentesque non a purus. Integer sapien mauris, feugiat quis massa tincidunt, pellentesque blandit ligula. Phasellus dignissim ipsum sollicitudin, tincidunt eros a, dignissim quam."
 
 const members = [
-  { name: "Chris Esperance", bio: sampleBio},
-  { name: "James Knight", bio: sampleBio},
-  { name: "Kilo Martin", bio: sampleBio},
-  { name: "Marcely Jean-Pierre", bio: sampleBio},
-  { name: "Remi Dhillon", bio: sampleBio},
-  { name: "Soumya Dhulekar", bio: sampleBio},
+  { name: "Quinton Alexander", bio: sampleBio},
   { name: "Yvonne Anderson", bio: sampleBio},
   { name: "Christina Anthony", bio: sampleBio},
-  { name: "Christine Walden", bio: sampleBio},
   { name: "Keane Cobb", bio: sampleBio},
   { name: "Kelly Conrad", bio: sampleBio},
-  { name: "Quinton Alexander", bio: sampleBio},
-  { name: "Randie Welles", bio: sampleBio},
   { name: "AJ de Leon", bio: sampleBio},
+  { name: "Remi Dhillon", bio: sampleBio},
+  { name: "Soumya Dhulekar", bio: sampleBio + sampleBio},
+  { name: "Chris Esperance", bio: sampleBio},
+  { name: "Marcely Jean-Pierre", bio: sampleBio},
+  { name: "James Knight", bio: sampleBio + sampleBio},
+  { name: "Kilo Martin", bio: sampleBio},
+  { name: "Christine Walden", bio: sampleBio},
+  { name: "Randie Welles", bio: sampleBio},
 ]
 
 const Members = () => {
