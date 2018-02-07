0b53f2c6cd485f55d1e75a588463feebe15af163
AJ de Leon <ajdeleon1064@gmail.com>
2018-02-07 13:21:47 -0500

    update style of email link

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => (
   <div className="tc mv5 site-dark o-80">
-    <h3>For inquiries please reach out to <span className="site-blue">americanexpressimprov@gmail.com</span></h3>
+    <h3>For inquiries please reach out to <a href="mailto:americanexpressimprov@gmail.com" className="site-blue link">americanexpressimprov@gmail.com</a></h3>
   </div>
 )
 
