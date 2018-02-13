b08368463e351c836499963801bec2517558b4b9
AJ de Leon <ajdeleon1064@gmail.com>
2018-02-12 23:09:38 -0500

    add assets index to manage all imports of images

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import smallBanner from './amex_banner_small.png'
+import lit from './lit_march2_small.png'
+import kelly from './kelly_large.png'
+
+export default {
+  smallBanner,
+  lit,
+  kelly
+}
