4262daea1503eb8fc90c08a639db478cb02ac8b4
AJ de Leon <ajdeleon1064@gmail.com>
2018-02-19 11:51:56 -0500

    add christina bio and image

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,11 @@
 import smallBanner from './amex_banner_small.png'
 import lit from './lit_march2_small.png'
 import kelly from './kelly_large.png'
+import christina from './christina_small.png'
 
 export default {
   smallBanner,
   lit,
-  kelly
+  kelly,
+  christina
 }
