fa351076f0d16b9c569cc34945399e471842347c
AJ de Leon <ajdeleon1064@gmail.com>
2018-02-24 15:49:13 -0500

    refactor video for easier adding of videos

--- src/components/media/Media.js
+++ src/components/media/Media.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
+//todo: pull this in dynamically from youtube
+const videos = [
+  {title: "Opening Video", src: "https://www.youtube.com/embed/V_jbj97HiPI"},
+  {title: "Food Porn", src: "https://www.youtube.com/embed/DWhVVbqiJzA"}
+]
+
 const Media = () => {
   return (
     <div className="mt4 tc">
       <p className="site-dark f4">Media</p>
-      <iframe src="https://www.youtube.com/embed/V_jbj97HiPI" title="Opening Video" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+      <div>
+        {videos.map(video => {
+          return <iframe src={video.src} className="ph2 pv1" title={video.title} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+        })}
+      </div>
     </div>
   )
 }
--- src/components/shows/Shows.js
+++ src/components/shows/Shows.js
@@ -6,7 +6,6 @@ import banner from '../../assets/amex_banner_small.png'
 import ShowListItem from './ShowListItem'
 
 const shows = [
-  { imgSource: banner, alt: "Family Business", showName: "Family Business", showDate: "2/23/18 - 10PM", location: "Philly Improv Theater", ticketLink: "https://phitcomedy.ticketleap.com/family-business-featuring-american-express-02-23-2018/" },
   { imgSource: litSketch, alt: "Most Lit Sketch Show", showName: "The Most Lit Sketch Show in the Planet", showDate: "3/2/18 - 7PM", location: "Good Good Comedy Theater", ticketLink: "https://www.facebook.com/events/202499530487421/" },
   { imgSource: banner, alt: "Family Business", showName: "Family Business", showDate: "3/23/18 - 10PM", location: "Philly Improv Theater", ticketLink: "https://www.phitcomedy.com/shows/" },
   { imgSource: banner, alt: "Family Business", showName: "Family Business", showDate: "4/27/18 - 10PM", location: "Philly Improv Theater", ticketLink: "https://www.phitcomedy.com/shows/" },
