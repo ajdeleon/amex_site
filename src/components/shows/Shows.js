450b3ea7002776e4f867b72ce91b777f429da360
AJ de Leon <ajdeleon1064@gmail.com>
2018-03-03 13:54:53 -0500

    remove passed show

--- src/components/shows/Shows.js
+++ src/components/shows/Shows.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-import litSketch from '../../assets/lit_march2_small.png'
 import banner from '../../assets/amex_banner_small.png'
 
 import ShowListItem from './ShowListItem'
 
 const shows = [
-  { imgSource: litSketch, alt: "Most Lit Sketch Show", showName: "The Most Lit Sketch Show in the Planet", showDate: "3/2/18 - 7PM", location: "Good Good Comedy Theater", ticketLink: "https://www.facebook.com/events/202499530487421/" },
   { imgSource: banner, alt: "Family Business", showName: "Family Business", showDate: "3/23/18 - 10PM", location: "Philly Improv Theater", ticketLink: "https://www.phitcomedy.com/shows/" },
   { imgSource: banner, alt: "Family Business", showName: "Family Business", showDate: "4/27/18 - 10PM", location: "Philly Improv Theater", ticketLink: "https://www.phitcomedy.com/shows/" },
   { imgSource: banner, alt: "Family Business", showName: "Family Business", showDate: "5/25/18 - 10PM", location: "Philly Improv Theater", ticketLink: "https://www.phitcomedy.com/shows/" },
