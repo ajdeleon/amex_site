ad84e1aa3f39d6ad9c17726f7c8e10b6f513544c
AJ de Leon <ajdeleon1064@gmail.com>
2018-03-16 17:36:47 -0400

    update sizing of logo for better responsiveness

--- src/components/Nav2.js
+++ src/components/Nav2.js
@@ -6,7 +6,7 @@ import banner from '../assets/amex_banner_small.png'
 const Nav2 = () => {
   return (
     <nav className="db dt-l w-100 border-box pa3 ph5-l">
-      <a className="db dtc-l v-mid link w-30 w-10-l mb2 mb0-l pointer center dim" href="/" title="Home">
+      <a className="db dtc-l v-mid link w5 w-10-l mb2 mb0-l pointer center dim" href="/" title="Home">
         <span><img src={banner} alt="AMEX Home"/></span>
       </a>
       <div className="db dtc-l v-mid w-100 w-75-l tc tr-l">
