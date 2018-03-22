38395dd3e32c0d253dfe14305790cdd554879372
AJ de Leon <ajdeleon1064@gmail.com>
2018-03-21 23:28:40 -0400

    add new line for date and time and separate into two distinct props

--- src/components/shows/ShowListItem.js
+++ src/components/shows/ShowListItem.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const ShowListItem = ({ imgSource, alt, showName, location, ticketLink, showDate }) => (
+const ShowListItem = ({ imgSource, alt, showName, location, ticketLink, showDate, showTime }) => (
   <li
     className="flex items-center lh-copy pa3 ph1-l bb b--black-20">
       <img className="w2 h2 w3-ns h3-ns br-100" src={imgSource} alt={alt}/>
       <div className="pl3 flex-auto">
-        <span className="f6 db b site-dark o-80">{showName}: {showDate}</span>
+        <span className="f6 db b site-dark o-80">{showName}</span>
         <span className="f6 db site-dark o-80">{location}</span>
+        <span className="f6 db site-dark o-80">{showDate} - {showTime}</span>
       </div>
       <div>
         <a href={ticketLink} target="blank" className="f5 link site-blue hover-site-yellow">Tickets</a>
@@ -19,9 +20,13 @@ ShowListItem.propTypes = {
   imgSource: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
   showName: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
+  location: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.object.isRequired
+  ]),
   ticketLink: PropTypes.string.isRequired,
-  showDate: PropTypes.string.isRequired
+  showDate: PropTypes.string.isRequired,
+  showTime: PropTypes.string.isRequired
 }
 
 export default ShowListItem
