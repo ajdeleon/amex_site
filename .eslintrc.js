c978d50763f33e18b7df00345b4d09a3a554a9c1
AJ de Leon <ajdeleon1064@gmail.com>
2018-04-27 20:40:58 -0400

    update to ignore react import but never used

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,22 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true,
     },
-    "extends": "eslint:recommended",
-  "parserOptions": {
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
-        }
-    },
-    "plugins": [
-        "react"
-    ],
-  "rules": {
-        "react/jsx-uses-vars": [2],
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
-};
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-uses-vars': [2],
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+  },
+}
