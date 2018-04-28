a424a82fde65a9d8ac0f58c692842ee5168b003f
AJ de Leon <ajdeleon1064@gmail.com>
2018-04-27 20:41:13 -0400

    remove past show

--- src/components/shows/Shows.js
+++ src/components/shows/Shows.js
@@ -4,14 +4,36 @@ import ShowListItem from './ShowListItem'
 import images from '../../assets'
 
 const shows = [
-  { imgSource: images.smallBanner, alt: "Family Business", showName: "Family Business", showDate: "4/27/18", showTime:"10PM", location: "Philly Improv Theater", ticketLink: "https://www.phitcomedy.com/shows/" },
-  { imgSource: images.charmCity, alt: "Charm City Comedy Project", showName: "Charm City Comedy Project", showDate: "5/5/18", showTime:"10PM", location: <a className="site-red link b dim" href='https://www.facebook.com/zissimosbar/'>Zissimos Bar</a>, ticketLink: "https://www.charmcitycomedyproject.com/festival" },
-  { imgSource: images.smallBanner, alt: "Family Business", showName: "Family Business", showDate: "5/25/18", showTime:"10PM", location: "Philly Improv Theater", ticketLink: "https://www.phitcomedy.com/shows/" },
+  {
+    imgSource: images.charmCity,
+    alt: 'Charm City Comedy Project',
+    showName: 'Charm City Comedy Project',
+    showDate: '5/5/18',
+    showTime: '10PM',
+    location: (
+      <a
+        className="site-red link b dim"
+        href="https://www.facebook.com/zissimosbar/"
+      >
+        Zissimos Bar
+      </a>
+    ),
+    ticketLink: 'https://www.charmcitycomedyproject.com/festival',
+  },
+  {
+    imgSource: images.smallBanner,
+    alt: 'Family Business',
+    showName: 'Family Business',
+    showDate: '5/25/18',
+    showTime: '10PM',
+    location: 'Philly Improv Theater',
+    ticketLink: 'https://www.phitcomedy.com/shows/',
+  },
 ]
 
 const Shows = () => {
   this.renderList = () => {
-    return shows.map((show) => {
+    return shows.map(show => {
       return <ShowListItem {...show} key={show.alt + show.showDate} />
     })
   }
